11357f57bfa2388d67aa6c9b1d860649ecdbd159
Sam <student979@live.com>
2017-02-11 19:53:53 -0500

    asdas

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sam/twoControllerTeleopv4.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sam/twoControllerTeleopv4.java
@@ -124,7 +124,7 @@ public class twoControllerTeleopv4 extends OpMode {
 
     private boolean ballSensed = false;
     boolean updatePWR = false;
-//sdfsdf
+//wood
 
     class getRPM extends TimerTask {
         @Override
