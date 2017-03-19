0f9a18e343e66b42a25f6eb41cf74f5ccc67f6c5
Mrinali <mrinalinakhre.7@gmail.com>
2017-03-19 19:16:10 -0400

    Shooter in autonomous fixed Merge branch 'master' of https://github.com/PerryStyle/ftc_app
    # Conflicts:
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mrinali/DriveToBeaconsBlue.java
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mrinali/DriveToBeaconsBlue1.java
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mrinali/DriveToBeaconsBlue3.java
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mrinali/DriveToBeaconsBlueShoot.java
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mrinali/DriveToBeaconsRed.java
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mrinali/DriveToBeaconsRed1.java
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mrinali/DriveToBeaconsRedShoot.java
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mrinali/OldAutonomous/DriveToBeaconsBlue1.java
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mrinali/OldAutonomous/DriveToBeaconsRed1.java
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mrinali/ShootDriveToCenter.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mrinali/AutonomousActionsColor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mrinali/AutonomousActionsColor.java
@@ -756,7 +756,7 @@ public class AutonomousActionsColor extends LinearOpMode {
         leftMotor.setPower(.1);
         rightMotor.setPower(.1);
 
-        while (opMode.opModeIsActive() && getcmUltrasonic(rangeSensor) > 13) {
+        while (opMode.opModeIsActive() && getcmUltrasonic(rangeSensor) > 12) {
             telemetry.addData("Front range", getcmUltrasonic(rangeSensor));
             telemetry.addData("Light", lightSensor.getLightDetected());
             telemetry.addData("Angle", IMUheading());
